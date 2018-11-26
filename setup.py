70a7450ce5eb2a4ab0af082592c03f23471652d8
Vladimir Goncharov <amatanhead@yandex-team.ru>
2018-11-26 21:39:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
 setup(
-   name='sphinx-a4doc',
-   version='1.0.0',
-   description='Sphinx domain and autodoc for Antlr4 grammar files',
-   author='Vladimir Goncharov',
-   author_email='dev.zelta@gmail.com',
-   install_requires=[
-       'sphinx>=1.8.0,<2.0.0',
-       'antlr4-python3-runtime==4.7.1',
-       'PyYAML'
-   ],
+    name='sphinx-a4doc',
+    version='1.0.0',
+    description='Sphinx domain and autodoc for Antlr4 grammar files',
+    author='Vladimir Goncharov',
+    author_email='dev.zelta@gmail.com',
+    packages=[
+        'sphinx_a4doc'
+    ],
+    install_requires=[
+        'sphinx>=1.8.0,<2.0.0',
+        'antlr4-python3-runtime==4.7.1',
+        'PyYAML'
+    ],
 )
