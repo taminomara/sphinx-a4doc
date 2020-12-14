7d34cdd77d65ed56c613af2c0c07c557209f0a89
Vladimir Goncharov <amatanhead@yandex-team.ru>
2020-12-14 19:21:20 +0300

    Code style

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
-setup(project_urls={
+setup(
+    project_urls={
         'Documentation': 'https://amatanhead.github.io/sphinx-a4doc/',
         'Source': 'https://github.com/AmatanHead/sphinx-a4doc',
         'Tracker': 'https://github.com/AmatanHead/sphinx-a4doc/issues',
     },
-    use_scm_version={"local_scheme": "no-local-version"})
+    use_scm_version={
+        "local_scheme": "no-local-version"
+    }
+)
