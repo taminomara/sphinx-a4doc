97214cb1a4b6c55c716c62225472ad821dd14ce7
Tamika Nomara <taminomara@gmail.com>
2025-09-28 04:52:13 +0400

    Update antlr to the latest release

--- sphinx_a4doc/syntax/gen/syntax/ANTLRv4ParserListener.py
+++ sphinx_a4doc/syntax/gen/syntax/ANTLRv4ParserListener.py
@@ -1,6 +1,6 @@
-# Generated from sphinx_a4doc/syntax/ANTLRv4Parser.g4 by ANTLR 4.9.2
+# Generated from ANTLRv4Parser.g4 by ANTLR 4.13.2
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .ANTLRv4Parser import ANTLRv4Parser
 else:
     from ANTLRv4Parser import ANTLRv4Parser
